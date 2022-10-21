53b077082199d6396dd500ae551dc9cf620b4ba7
Truongkhoa1999 <113099964+Truongkhoa1999@users.noreply.github.com>
2022-10-22 01:53:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   // city search form
   const fetchCity = (e) => {
     e.preventDefault();
-    axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${citySearch}`)
+    axios.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${citySearch}`)
       .then((res) => {
         setCityData(res.data[0]);
         setCitySearch('');
